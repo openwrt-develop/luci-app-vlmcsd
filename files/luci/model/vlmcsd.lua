d9012f27af8150b9ecd46c0b68750d838c8b1bb5
panda-mute <wxuzju@gmail.com>
2017-02-06 13:14:37 +0800

    Update vlmcsd luci

--- files/luci/model/vlmcsd.lua
+++ files/luci/model/vlmcsd.lua
@@ -1,4 +1,7 @@
-local m, s
+local fs = require "nixio.fs"
+local util = require "nixio.util"
+
+local m, s, o
 
 local running=(luci.sys.call("pidof vlmcsd > /dev/null") == 0)
 if running then	
@@ -8,35 +11,20 @@ else
 end
 
 s = m:section(TypedSection, "vlmcsd", "")
-s.addremove = false
 s.anonymous = true
 
-enable = s:option(Flag, "enabled", translate("Enable"))
-enable.rmempty = false
-function enable.cfgvalue(self, section)
-	return luci.sys.init.enabled("vlmcsd") and self.enabled or self.disabled
-end
+s:tab("basic", translate("Basic Setting"))
+s:taboption("basic", Flag, "enable", translate("Start"))
+s:taboption("basic", Flag, "use_conf_file", translate("Using Config File"))
+o = s:taboption("basic", Value, "port", translate("Local Port"))
+o.datatype = "port"
+o.default = 1688
+o.placeholder = 1688
 
 local hostname = luci.model.uci.cursor():get_first("system", "system", "hostname")
 
-autoactivate = s:option(Flag, "autoactivate", translate("Auto activate"))
+autoactivate = s:taboption("basic", Flag, "autoactivate", translate("Auto activate"))
 autoactivate.rmempty = false
-
-local file = "/etc/vlmcsd.ini"
-config = s:option(TextValue, "config", translate("Config file"), translate("This file is /etc/vlmcsd.ini."), "")
-config.size = 100
-config.rows = 20
-config.wrap = "off"
-
-function config.cfgvalue(self, section)
-	return nixio.fs.readfile(file)
-end
-
-function config.write(self, section, value)
-	value = value:gsub("\r\n?", "\n")
-	nixio.fs.writefile(file, value)
-end
-
 function autoactivate.write(self, section, value)
 	if value == "1" then
 		luci.sys.call("sed -i '/srv-host=_vlmcs._tcp.lan/d' /etc/dnsmasq.conf")
@@ -47,6 +35,25 @@ function autoactivate.write(self, section, value)
 		luci.sys.call("/etc/init.d/dnsmasq restart")
 	end
 	Flag.write(self, section, value)
+end
+
+s:tab("config", translate("Config File"))
+conf = s:taboption("config", Value, "editconf", nil, translate("开头的数字符号（＃）或分号（;）的每一行被视为注释；删除（;）启用指定选项。"))
+conf.template = "cbi/tvalue"
+conf.rows = 20
+conf.wrap = "off"
+function conf.cfgvalue(self, section)
+	return fs.readfile("/etc/vlmcsd.ini") or ""
+end
+function conf.write(self, section, value)
+	if value then
+		value = value:gsub("\r\n?", "\n")
+		fs.writefile("/tmp/vlmcsd.ini", value)
+		if (luci.sys.call("cmp -s /tmp/vlmcsd.ini /etc/vlmcsd.ini") == 1) then
+			fs.writefile("/etc/vlmcsd.ini", value)
+		end
+		fs.remove("/tmp/vlmcsd.ini")
+	end
 end
 
 return m
